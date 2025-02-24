a7108c5d3a4a182df95757c7bd17b6b6af6fb40a
Siddhu054 <siddharthaballa123@gmail.com>
2025-02-24 21:47:55 +0530

    Create api.js

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Add a request interceptor to add the auth token to requests
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    console.log("Using token:", token ? "Present" : "None");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error("Request interceptor error:", error);
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor to handle errors
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error("Response Error:", error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error("Request Error:", error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error("Error:", error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
