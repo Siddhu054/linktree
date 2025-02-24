1ccc95b49a50f2e27f1784453d0d3125bec7a271
Siddhu054 <siddharthaballa123@gmail.com>
2025-02-24 23:40:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,10 @@ app.get("/test", (req, res) => {
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
